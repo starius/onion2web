87f8c20f2a29afaf45df8faad801d10dee3fe20a
la8 <la82@protonmail.ch>
2017-08-17 19:26:14 -0400

    add Report the site link

--- onion2web.lua
+++ onion2web.lua
@@ -58,6 +58,8 @@ that you have understood:
 </form>
 </center>
 
+<a href="https://ahmia.fi/blacklist/">Report the site</a>
+
 <a href="https://github.com/starius/onion2web">
 <img style="position: absolute; top: 0; right: 0; border: 0;"
 src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
